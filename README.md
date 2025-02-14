c4095944b8568c43bb38131c33fca3ae257b5b80
stim-ca <stim-ca@timewelltech.ca>
2025-02-14 14:21:13 -0800

    feat: init project
    build module with Catesta

---
+++ README.md
@@ -0,0 +1,40 @@
+# ZabbixAPI
+
+## Synopsis
+
+<!-- Enter a synopsis -->
+
+## Description
+
+<!-- Enter a description -->
+
+## Why
+
+<!-- Short reason you created the project -->
+
+## Getting Started
+
+### Prerequisites
+
+<!-- list any prerequisites -->
+
+### Installation
+
+```powershell
+# how to install ZabbixAPI
+
+```
+
+### Quick start
+
+#### Example1
+
+```powershell
+# command showing how to use your project
+
+```
+
+## Author
+
+stim-ca
+
---
+++ src/Tests/Unit/Public/Get-HelloWorld.Tests.ps1
@@ -0,0 +1,38 @@
+BeforeDiscovery {
+    Set-Location -Path $PSScriptRoot
+    $ModuleName = 'ZabbixAPI'
+    $PathToManifest = [System.IO.Path]::Combine('..', '..', '..', $ModuleName, "$ModuleName.psd1")
+    #if the module is already in memory, remove it
+    Get-Module $ModuleName -ErrorAction SilentlyContinue | Remove-Module -Force
+    Import-Module $PathToManifest -Force
+}
+
+InModuleScope 'ZabbixAPI' {
+    Describe 'Get-HelloWorld Public Function Tests' -Tag Unit {
+        BeforeAll {
+            $WarningPreference = 'SilentlyContinue'
+            $ErrorActionPreference = 'SilentlyContinue'
+        } #beforeAll
+        Context 'Error' {
+
+            # It 'should ...' {
+
+            # } #it
+
+        } #context_Error
+        Context 'Success' {
+
+            BeforeEach {
+                Mock -CommandName Get-Day -MockWith {
+                    'Friday'
+                } #endMock
+            } #beforeEach
+
+            It 'should return the expected results' {
+                Get-HelloWorld | Should -BeExactly 'Hello, happy Friday World!'
+            } #it
+
+        } #context_Success
+    } #describe_Get-HelloWorld
+} #inModule
+
